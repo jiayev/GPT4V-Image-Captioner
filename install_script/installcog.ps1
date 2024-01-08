1ff683ed449dbc74afa00aa60b65089d07e404a8
周公不解梦 <80639626+SleeeepyZhou@users.noreply.github.com>
2024-01-08 18:55:23 +0800

    Fix requirements

--- install_script/installcog.ps1
+++ install_script/installcog.ps1
@@ -6,6 +6,7 @@ Write-Output "Installing deps..."
 pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 --extra-index-url https://download.pytorch.org/whl/cu118
 pip install -U -I --no-deps xformers==0.0.22
 pip install bitsandbytes==0.41.1
+pip install ./install_script/deepspeed-0.11.2+8ce7471-py3-none-any.whl
 pip install -r ./install_script/require.txt
 
 Write-Output "Install completed"
