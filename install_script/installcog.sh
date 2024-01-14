ea7093e18b2c133da0c07b649d9cf6d7e86c29fb
facok <128763816+facok@users.noreply.github.com>
2024-01-14 09:44:09 +0800

    cu121 fix

--- install_script/installcog.sh
+++ install_script/installcog.sh
@@ -3,7 +3,7 @@ export HF_HOME="huggingface"
 source myenv/bin/activate
 
 echo "Installing deps..."
-pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
 pip install -U -I --no-deps xformers==0.0.22
 pip install bitsandbytes==0.41.1
 pip install deepspeed
