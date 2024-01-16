b3b98bd837fb7b973ce4b05591668b61d3c38149
周公不解梦 <sleeeepyzhou@yeah.net>
2024-01-16 21:18:38 +0800

    default API optimize

--- start_linux_mac.sh
+++ start_linux_mac.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Activate the virtual environment
 source myenv/bin/activate
 
-# Run Python script
 python gpt-caption.py
 
-# Wait for a user input to pause the script
 read -p "Press any key to continue . . . " -n1 -s
 echo
--- start_windows.bat
+++ start_windows.bat
@@ -1,21 +1,8 @@
 @echo off
 set url=http://127.0.0.1:8848
-set max_attempts=40
 
 call myenv\Scripts\activate
 start /B python gpt-caption.py
-:LOOP
-ping -n 1 %url% >nul
-if errorlevel 1 (
-    echo Waiting for %url% to respond...
-    timeout /nobreak /t 2 >nul
-    set /a max_attempts -=1
-    if %max_attempts% gtr 0 goto :LOOP
-    echo Maximum attempts reached. Exiting.
-    exit /b 1
-) else (
-    echo %url% is reachable. Opening...
-    start "" %url%
-    exit /b 0
-)
+timeout /nobreak /t 2 >nul
+start "" %url%
 pause
