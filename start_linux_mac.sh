4650be1ea04d9669f7e1404aabbb256271d22e3e
周公不解梦 <sleeeepyzhou@yeah.net>
2024-01-16 21:21:49 +0800

    Optimize startup

--- start_linux_mac.sh
+++ start_linux_mac.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+url="http://127.0.0.1:8848"
 
 source myenv/bin/activate
 
-python gpt-caption.py
+python gpt-caption.py &
+sleep 2
+xdg-open "$url"
 
 read -p "Press any key to continue . . . " -n1 -s
 echo
