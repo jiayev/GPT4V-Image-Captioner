8a16435ddbc097110d09c9c11bf71cd614af3b8a
ananosleep <1368882154@qq.com>
2024-03-25 23:12:47 +0800

    Linux: add pip mirror

--- install_linux_mac.sh
+++ install_linux_mac.sh
@@ -17,6 +17,20 @@ python3 -m venv myenv
 # Activate the virtual environment
 source myenv/bin/activate
 
+# Ping google to decide if use mirror
+target_url="www.google.com"
+timeout=3000
+ping -c 1 -W $timeout $target_url -w 3 > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Use CN"
+    export PIP_DISABLE_PIP_VERSION_CHECK=1
+    export PIP_NO_CACHE_DIR=1
+    export PIP_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
+else
+    echo "Use default"
+fi
+
 # Upgrade pip to the latest version
 pip install --upgrade pip
 
